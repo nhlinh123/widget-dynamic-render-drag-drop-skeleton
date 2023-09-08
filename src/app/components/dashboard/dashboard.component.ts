2c63816b52ea227889445efd0959d5827b5618a8
nhlinh123 <67900783+nhlinh123@users.noreply.github.com>
2023-09-08 16:31:00 +0700

    2 files modified

--- src/app/components/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard.component.ts
@@ -51,7 +51,6 @@ export class DashboardComponent
               );
             }
           });
-
           return widgetPositions;
         })
       )
--- src/app/components/drag-drop-generator.component.ts
+++ src/app/components/drag-drop-generator.component.ts
@@ -13,73 +13,50 @@ import { WidgetService } from './base-widget.component';
 @Component({
   selector: 'app-drag-drop-generator',
   template: `
-  <ng-container
-  [ngTemplateOutlet]="template"
-  [ngTemplateOutletContext]="{ datas: widgetList }"
->
-</ng-container>
 
-<ng-template #template let-datas="datas">
   <div class="container mt-3 mb-3">
-    <div
-      cdkDropList
-      class="grid-container"
-      #list="cdkDropList"
-      [cdkDropListConnectedTo]="[list]"
-      [cdkDropListData]="datas"
-      (cdkDropListDropped)="onDrop($event)"
-    >
-      <ng-container *ngFor="let widgetPos of datas">
-        <app-widget-generator
-          [class]="{ customizable: isCustomizing }"
-          [widgetName]="widgetPos.name"
-          [config]="getComponentConfig(widgetPos.name)"
-          [cdkDragDisabled]="!isCustomizing"
-          (cdkDragStarted)="onDrag($event)"
-          (cdkDragEnded)="onDrop($event)"
-          cdkDrag
-        ></app-widget-generator>
-      </ng-container>
-    </div>
-  </div>
-</ng-template>
-
+      <div
+        cdkDropList
+        class="grid-container"
+        #list="cdkDropList"
+        [cdkDropListData]="widgetList"
+        (cdkDropListDropped)="onDrop($event)"
+      >
+        <ng-container *ngFor="let widgetPos of widgetList">
+          <app-widget-generator
+            [class]="{ customizable: isCustomizing }"
+            [widgetName]="widgetPos.name"
+            [config]="getComponentConfig(widgetPos.name)"
+            [cdkDragDisabled]="!isCustomizing"
+            (cdkDragStarted)="onDrag($event)"
+            (cdkDragEnded)="onDrop($event)"
+            cdkDrag
+          ></app-widget-generator>
+        </ng-container>
+      </div>
+    </div>  
   `,
   styles: [
-    `
-    .grid-container {
-      display: grid;
-      grid-template-columns: repeat(4, 1fr);
-      grid-auto-rows: 100px;
-      gap: 10px;
-    }
-    
-    .customizable {
-      border: 1px dotted #ccc;
-    }
-    
-    .cdk-drag-placeholder {
-      border: 1px dashed #aaa;
-      transition: transform 250ms cubic-bezier(0, 0, 0.2, 1);
-    }
-    
-    .placeholder {
-      width: 100%;
-      height: 100%;
-      background-color: rgba(0, 0, 0, 0.1);
-      border: 2px solid rgba(0, 0, 0, 0.3);
-    }
-    `,
+    '.grid-container { display: grid; grid-template-columns: repeat(4, 1fr);grid-auto-rows: 100px;gap: 10px;}',
+    '.customizable { border: 1px dotted #ccc;}',
+    '.cdk-drag-placeholder {border: 1px dashed #aaa;transition: transform 250ms cubic-bezier(0, 0, 0.2, 1); }',
+    '.placeholder { width: 100%; height: 100%; background-color: rgba(0, 0, 0, 0.1); border: 2px solid rgba(0, 0,  0, 0.3); }',
   ],
 })
 export class DragDropGeneratorComponent implements OnInit {
-  @Input() widgetList: any[] = [];
+  _widgetList: any[] = [];
+  @Input()
+  set widgetList(value) {
+    console.log(value);
+    if (value) this._widgetList = value;
+  }
+  get widgetList() {
+    return this._widgetList;
+  }
   @Input() isCustomizing: boolean = false;
   @Output() dragEvent: EventEmitter<any> = new EventEmitter<any>();
   @Output() dropEvent: EventEmitter<any> = new EventEmitter<any>();
-  constructor(
-    private widgetService: WidgetService
-  ) {}
+  constructor(private widgetService: WidgetService) {}
 
   ngOnInit(): void {}
 
